7230b7532be76a1b38d94963f86b500999b823c1
Robert Brünig <robert.bruenig@gmail.com>
2017-05-19 12:41:17 +0200

    whoops

--- rtp-relay/main.cpp
+++ rtp-relay/main.cpp
@@ -10,20 +10,15 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    unsigned char test = 0b00111000;
+    UdpRelay rtpRelay;
+    RtpAnalyzer rtpAnalyzer;
+    QObject::connect(&rtpRelay, &UdpRelay::receivedPacket, &rtpAnalyzer, &RtpAnalyzer::analyzePacket);
+    rtpRelay.start(7070, QHostAddress::LocalHost, 7080);
 
-    qDebug() << test;
-    qDebug() << extractBits(test, 2, 3);
-
-//    UdpRelay rtpRelay;
-//    RtpAnalyzer rtpAnalyzer;
-//    QObject::connect(&rtpRelay, &UdpRelay::receivedPacket, &rtpAnalyzer, &RtpAnalyzer::analyzePacket);
-//    rtpRelay.start(7070, QHostAddress::LocalHost, 7080);
-
-//    UdpRelay rtcpRelay;
-//    RtcpAnalyzer rtcpAnalyzer;
-//    QObject::connect(&rtcpRelay, &UdpRelay::receivedPacket, &rtcpAnalyzer, &RtcpAnalyzer::analyzePacket);
-//    rtcpRelay.start(7071, QHostAddress::LocalHost, 7081);
+    UdpRelay rtcpRelay;
+    RtcpAnalyzer rtcpAnalyzer;
+    QObject::connect(&rtcpRelay, &UdpRelay::receivedPacket, &rtcpAnalyzer, &RtcpAnalyzer::analyzePacket);
+    rtcpRelay.start(7071, QHostAddress::LocalHost, 7081);
 
     return a.exec();
 }
